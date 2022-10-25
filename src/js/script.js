333dfa39d16c11de4182b592fcae8b06068372fc
Massimiliano Riva <m.riva@crif.com>
2022-10-25 09:10:08 +0200

    navbar almost completed + jumbotron

--- src/js/script.js
+++ src/js/script.js
@@ -7,11 +7,6 @@ $(function () {
 
     // Loop through the buttons and add the active class to the current/clicked button
     for (let i = 0; i < btns.length; i++) {
-        btns[i].addEventListener("mouseover", function() {
-            let current = document.getElementsByClassName("active");
-            current[0].className = current[0].className.replace(" active", "");
-            this.className += " active";
-        });
         btns[i].addEventListener("click", function() {
             let current = document.getElementsByClassName("active");
             current[0].className = current[0].className.replace(" active", "");
