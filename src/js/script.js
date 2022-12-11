ad20e7ba0b675eb05d6f2be0b97e4ebb1469f836
massimiliano96 <48362794+massimiliano96@users.noreply.github.com>
2022-12-11 18:28:29 +0100

    Content dynamic loading (#2)
    * Ajax first commit
    
    * images resized and loadDescription started
    
    * dynamic loading of description
    
    * restoring index redirect
    
    Co-authored-by: Massimiliano Riva <m.riva@crif.com>

--- src/js/script.js
+++ src/js/script.js
@@ -14,3 +14,126 @@ $(function () {
         });
     }
 });
+
+$(function () {
+    // In Firefox and Safari, the click event doesn't retain the focus
+    // on the clicked button. Therefore, the blur event will not fire on
+    // user clicking somewhere else in the page and the blur event handler
+    // which is set up above will not be called.
+    // Refer to issue #28 in the repo.
+    // Solution: force focus on the element that the click event fired on
+    $("#navbarToggle").click(function (event) {
+        $(event.target).focus();
+    });
+});
+
+
+(function(global) {
+    let fm = {};
+    let homeHtml = "snippets/home-snippet.html"
+    let allServicesUrl = "../data/services.json"
+    let serviceHtml = "snippets/service-snippet.html"
+    let servicesTitleHtml = "snippets/services-title-snippet.html"
+
+    // Convenience function for inserting innerHTML for 'select'
+    let insertHtml = function (selector, html) {
+        let targetElem = document.querySelector(selector);
+        targetElem.innerHTML = html;
+    };
+
+    let showLoading = function (selector) {
+        let html = "<div class='text-center'>";
+        html += "<img src='../images/loading.gif'></div>";
+        insertHtml(selector, html);
+    };
+
+    // Return substitute of '{{propName}}' with propValue in given 'string'
+    let insertProperty = function (string, propName, propValue) {
+        let propToReplace = "{{" + propName + "}}";
+        string = string.replace(new RegExp(propToReplace, "g"), propValue);
+        return string;
+    }
+
+    // On page load, show home view
+    document.addEventListener("DOMContentLoaded", function (event){
+        // On first load, show home view
+        showLoading("#main-content");
+            $ajaxUtils.sendGetRequest(
+                homeHtml,
+                function (homeHtml) {
+                    // Retrieve single service snippet
+                    $ajaxUtils.sendGetRequest(
+                        servicesTitleHtml,
+                        function (servicesTitleHtml) {
+                            $ajaxUtils.sendGetRequest(allServicesUrl,
+                                function (services) {
+                                $ajaxUtils.sendGetRequest(serviceHtml,
+                                    function (serviceHtml) {
+                                        let servicesViewHtml = buildHomePageHtml(services, servicesTitleHtml, homeHtml, serviceHtml);
+                                        insertHtml("#main-content", servicesViewHtml);
+                                    },
+                                    false);
+                                },
+                                        true);
+                                },
+                                false);
+                        },
+                false);
+    })
+
+
+    //Using categories data and snippets html build services view HTML to be inserted into page
+    function buildHomePageHtml(services, servicesTitleHtml, homeHtml, serviceHtml){
+        let finalHtml = homeHtml;
+        finalHtml += servicesTitleHtml;
+        finalHtml += "<div id=\"overflow-wrapper\">"
+        finalHtml += "<div class=\"container services-container\">"
+
+        //Loop over services
+        for(let i = 0; i < services.length; i++) {
+            //Insert service values
+            let html = serviceHtml;
+            let name = "" + services[i].name;
+            let short_name = services[i].short_name;
+            let id = services[i].id;
+            html = insertProperty(html, "status", "");
+            html = insertProperty(html, "name", name);
+            html = insertProperty(html, "short_name", short_name);
+            html = insertProperty(html, "id", id);
+            finalHtml += html;
+        }
+        let html = "</div></div>";
+        finalHtml += html
+        finalHtml += "<div class=\"service-description\" id=\"service-description\">{{description}}</div>"
+        return finalHtml;
+    }
+
+   fm.loadDescription = function (id) {
+        let allServicesUrl = "../data/services.json";
+        //let html = document.getElementsByClassName("service-description")[id];
+
+       let html = document.getElementById('service-description');
+        html.innerHTML = "{{description}}";
+        html.style.display = "block";
+        $ajaxUtils.sendGetRequest(allServicesUrl,
+            function (services) {
+                for (let i = 0; i < services.length; i++) {
+                    if(i == id) {
+                        html = insertProperty(html.outerHTML, "description", services[i].description);
+                        let descriptionTitle = "<p>"+ services[i].name +"</p><br>"
+                        let selector = "#service-description";
+                        let finalHtml = descriptionTitle + html;
+                        insertHtml(selector, finalHtml);
+                        break;
+                    }
+                }
+            },
+            true);
+    }
+
+    fm.collapseDescription =  function  () {
+        document.getElementById('service-description').style.display = "none";
+    }
+
+    global.$fm = fm;
+})(window);
